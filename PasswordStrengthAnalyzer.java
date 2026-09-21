32527b054086b8b19f5b459cbd62836b7d25c823
Krishnal Patil <krishpatil0018@gmail.com>
2026-09-21 23:30:47 +0530

    Update PasswordStrengthAnalyzer.java

--- PasswordStrengthAnalyzer.java
+++ PasswordStrengthAnalyzer.java
@@ -6,7 +6,7 @@ public class PasswordStrengthAnalyzer {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=========================================");
+        System.out.println("==========================================");
         System.out.println("   PASSWORDS STRENGTH ANALYZER");
         System.out.println("========================================");
 
